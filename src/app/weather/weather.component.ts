2d42bde313393cdb451828b7fa832f3a84eb386b
Roshan Poudel <rpoudel912@gmail.com>
2023-04-07 08:54:14 -0600

    added weather model

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -30,6 +30,8 @@ export class WeatherComponent implements OnInit{
   });
   }
 
+ 
+
 
 
 }
--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, tap } from 'rxjs';
+import { Observable, catchError, tap, of } from 'rxjs';
+import { Weather } from 'src/model/Weather.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,30 @@ export class WeatherService {
   apiKey = '901114c1b9f712e0706bd9f005a1a380';
   apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-  getWeather(city: string) {
-    return this.http.get(`${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`);
-       
-  }
 
     // /** GET person type by id. Will 404 if id not found */
-    // getWeather(city: string): Observable<Weather> {
-    //   const url = `${this.personTypesUrl}/${id}`;
-    //   return this.http.get<PersonType>(url).pipe(
-    //     tap(_ => this.log(`fetched person type id=${id}`)),
-    //     catchError(this.handleError<PersonType>(`getPersonType id=${id}`))
-    //   );
-    // }
+    getWeather(city: string): Observable<Weather> {
+      const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+      return this.http.get<Weather>(url).pipe(
+        tap(_ => this.log(`fetched weather data`)),
+        catchError(this.handleError<Weather>(`getWeather`))
+      );
+    }
+      /** Log a PersonService message with the MessageService */
+  private log(message: string) {
+    console.log(message);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
